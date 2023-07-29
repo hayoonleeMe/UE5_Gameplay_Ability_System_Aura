0d046e3ee7e5f5a4e4c66d8cc37d556bd6aaa2fc
LeeHaYoon <htdobad@gmail.com>
2023-07-29 15:43:49 +0900

    Section 8 - Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
