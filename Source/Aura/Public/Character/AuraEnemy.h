335d4a0f1dce7500ca51370d55feebfb2708a2b0
LeeHaYoon <htdobad@gmail.com>
2023-09-09 15:45:54 +0900

    Section 15 - Combat Target

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -34,6 +34,8 @@ public:
 	virtual int32 GetPlayerLevel() override;
 	virtual UAnimMontage* GetAttackMontage_Implementation() override;
 	virtual void Die() override;
+	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
+	virtual AActor* GetCombatTarget_Implementation() const override;	
 	/* End Combat Interface */
 
 	UPROPERTY(BlueprintAssignable)
@@ -52,6 +54,9 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	float LifeSpan = 5.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Combat")
+	TObjectPtr<AActor> CombatTarget;
 
 protected:
 	virtual void BeginPlay() override;
