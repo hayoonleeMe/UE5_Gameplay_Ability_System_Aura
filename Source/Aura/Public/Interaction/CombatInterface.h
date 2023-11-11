bca18f3b2559e90b9398c66e1c99383cf3fb8597
hayoonlee <htdobad@gmail.com>
2023-11-11 23:18:58 +0900

    Section 22 - Level Up Interface Function

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -40,7 +40,8 @@ class AURA_API ICombatInterface
 	GENERATED_BODY()
 
 public:
-	virtual int32 GetPlayerLevel();
+	UFUNCTION(BlueprintNativeEvent)
+	int32 GetPlayerLevel();
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	FVector GetCombatSocketLocation(const FGameplayTag& SocketTag);
