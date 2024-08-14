d239a0047bde5aa17c53c034b1a7efa55a90a8bd
hayoonleeMe <htdobad@gmail.com>
2024-08-14 14:55:30 +0900

    Game Mode

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// Typical Parameters for Top Down Game
+	GetCharacterMovement()->bOrientRotationToMovement = true;	// 캐릭터 가속 방향으로 RotationRate를 적용해 회전시킴
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;	// 이동을 평면에 제한
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;	// 시작 시 캐릭터가 평면에 부착됨
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
