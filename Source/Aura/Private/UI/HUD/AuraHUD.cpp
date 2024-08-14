7b07e20ae168004d08580d4d2068939e1553b910
hayoonleeMe <htdobad@gmail.com>
2024-08-14 19:12:04 +0900

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (OverlayWidgetClass)
+	{
+		UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+		Widget->AddToViewport();
+	}
+}
