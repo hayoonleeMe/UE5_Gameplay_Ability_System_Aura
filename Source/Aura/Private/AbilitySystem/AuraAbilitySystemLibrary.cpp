9ba31fff3c51dd9f231c6288760f3796daa4cbd5
hayoonleeMe <htdobad@gmail.com>
2024-08-15 20:50:27 +0900

    Aura Ability System Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	// 이 함수는 대부분 위젯에서 호출되고, 위젯은 Local Player에만 존재하기 때문에 여기서 GetPlayerController()의 반환값은 Local Player의 Controller가 될 것이다.
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			if (AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>())
+			{
+				UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+				UAttributeSet* AS = PS->GetAttributeSet();
+				const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+				return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+			}
+		}
+	}
+	return nullptr;
+}
