4ddbb19aef9ee77b49fb085e38c9cd07e54789af
hayoonleeMe <htdobad@gmail.com>
2024-08-16 17:11:11 +0900

    Receiving Target Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -19,7 +19,15 @@ void UTargetDataUnderMouse::Activate()
 	}
 	else
 	{
-		// TODO : We are on the server, so listen for target data.
+		const FGameplayAbilitySpecHandle SpecHandle = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(SpecHandle, ActivationPredictionKey).AddUObject(this, &ThisClass::OnTargetDataReplicatedCallback);
+
+		// 아직 클라이언트가 보낸 TargetData가 서버에 도착하지 않았다면 올 때까지 기다린다.
+		if (!AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(SpecHandle, ActivationPredictionKey))
+		{
+			SetWaitingOnRemotePlayerData();
+		}
 	}
 }
 
@@ -34,7 +42,7 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 		PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
 
 		FGameplayAbilityTargetDataHandle DataHandle;
-		FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+		FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();	// DataHandle에서 TSharedPtr로 관리함
 		Data->HitResult = CursorHit;
 		DataHandle.Add(Data);
 
@@ -46,4 +54,14 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 			ValidData.Broadcast(DataHandle);
 		}
 	}	
+}
+
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag)
+{
+	// ASC에게 이제 TargetData를 받았으니 더이상 캐싱하지 말도록 전하고 캐시 데이터를 모두 비운다.
+	AbilitySystemComponent.Get()->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -3,6 +3,7 @@
 
 #include "AuraAssetManager.h"
 
+#include "AbilitySystemGlobals.h"
 #include "AuraGameplayTags.h"
 
 // UAuraAssetManager 클래스를 프로젝트의 Asset Manager로 등록하기 위해서 기존 AssetManager를 이 클래스로 캐스팅한 것을 반환하는 Get() 메소드를 구현해야 한다.
@@ -20,4 +21,8 @@ void UAuraAssetManager::StartInitialLoading()
 
 	// Asset Manager의 초기 로딩 시 C++ Native GameplayTags 초기화
 	FAuraGameplayTags::InitializeNativeGameplayTags();
+	
+	// This is required to use Target Data
+	// TargetDataScriptStructCache를 초기화 해주는 기능을 포함한다.
+	UAbilitySystemGlobals::Get().InitGlobalData();	
 }
